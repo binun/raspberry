68c88513a9ef74f5c0dfb7d9bafaeeef7b91817c
root <binun@cs.bgu.ac.il>
2016-11-03 12:58:37 +0200

    UniInstalll

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #/bin/bash -i
 
-apt-get -y update && apt-get -y upgrade && rpi-update
-apt-get -y install nc ent
+YUM_CMD=$(which yum)
+APT_GET_CMD=$(which apt-get)
+
+if [[ ! -z $YUM_CMD ]]; then
+	yum -y update && apt-get -y upgrade
+	yum -y install netcat ent
+elif [[ ! -z $APT_GET_CMD ]]; then
+	apt-get -y update && apt-get -y upgrade
+	apt-get -y install netcat ent
+else
+	echo "error can't install package"
+	exit 1;
+fi
+
+rpi-update
 
 cp ./options/new/sysctl.conf /etc
 gcc tcpserver.c -o tcpserver -lpthread
